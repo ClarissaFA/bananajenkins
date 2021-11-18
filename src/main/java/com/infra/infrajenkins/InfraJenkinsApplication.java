48d38edf921144eb24afc8295d1a9aa58869f7a8
Clarissa Farencena Arruda <cla.farencena@gmail.com>
2021-11-18 11:10:12 -0300

    criando banana jenkins

---
+++ src/main/java/com/infra/infrajenkins/InfraJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.infra.infrajenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraJenkinsApplication.class, args);
+	}
+
+}
